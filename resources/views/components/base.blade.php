b68f2689ee50fcc456f35c717ad816598ab3b70d
RIE <chip.014868@gmail.com>
2024-05-22 16:10:49 +0900

    管理画面新規作成

---
+++ resources/views/components/base.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script>
+        (function(d) {
+          var config = {
+            kitId: 'cbi4ocw',
+            scriptTimeout: 3000,
+            async: true
+          },
+          h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+        })(document);
+      </script>
+    <title>ベース</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/detail-style.css">
+</head>
+<body>
+    <header id="header">
+        <div class="hf_top"></div>
+        <div class="hf_bottom"></div>
+        <div><img src="img/logo.png" alt="じゃがじゃがみやざき"></div>
+    </header>
+{{ $slot }}
+    <footer id="footer">
+        <div class="hf_top"></div>
+        <div class="hf_bottom"></div>
+        <div class="copyright">&copy;Live93 2024</div>
+    </footer>
+</body>
+</html>
