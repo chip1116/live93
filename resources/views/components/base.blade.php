a0dba25a67847cf72580e9ca9fffe2c27f2fe997
t4keitez29 <fizz.f.cascade@gmail.com>
2024-05-23 11:50:07 +0900

    mypage.cssの修正

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -13,7 +13,7 @@
           h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
         })(document);
       </script>
-    <title>ベース</title>
+    <title>じゃがじゃがみやざき</title>
     <link rel="stylesheet" href="css/common.css">
     <link rel="stylesheet" href="css/style.css">
     @if(Request::routeIs('user.detail-screen'))
