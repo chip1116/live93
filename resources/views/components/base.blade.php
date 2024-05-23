01dc19850a14db958983baee4b5f8e406ac420b0
Kenzo <kenzo_55@icloud.com>
2024-05-23 13:21:17 +0900

    自分が作成したページのルーティングを行った

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="css/style.css">
     @if(Request::routeIs('user.detail-screen'))
     <link rel="stylesheet" href="css/detail-style.css">
-    @elseif(Request::routeIs('user.contact'))
+    @elseif(Request::routeIs('user.contact') || Request::routeIs('user.post') || Request::routeIs('user.login') || Request::routeIs('user.register'))
     <link rel="stylesheet" href="css/kim4page.css">
     @elseif(Request::routeIs('user.mypage'))
     <link rel="stylesheet" href="css/mypage.css">
--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -28,4 +28,4 @@
         </inner>
 
     </main>
-<x-base>
+</x-base>
