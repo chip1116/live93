ea153e511d09fe014556c400231c172e2d93036c
RIE <chip.014868@gmail.com>
2024-05-28 11:45:51 +0900

    user_id追加

--- database/migrations/2024_05_27_022212_create_stores_table.php
+++ database/migrations/2024_05_27_022212_create_stores_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('stores', function (Blueprint $table) {
             $table->id();
+            $table->user_id('bigInteger');
             $table->string('name',50);
             $table->string('postal-code',8);
             $table->string('address-level2');
