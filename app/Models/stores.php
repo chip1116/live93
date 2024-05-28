cad0801d28706324fd8a0b5ba3eadb264ca23d57
小林ひかり <h0904066@gmail.com>
2024-05-28 11:56:32 +0900

    テーブル作成しました〜

--- app/Models/stores.php
+++ app/Models/stores.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class stores extends Model
+class Stores extends Model
 {
     use HasFactory;
 }
