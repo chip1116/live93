0945656f2a8b9ae5cf1940fd2e9f9921672552b2
t4keitez29 <fizz.f.cascade@gmail.com>
2024-06-04 21:33:08 +0900

    いいね機能の追加（ボタン押すたびにデータベースと画像の切り替え）

--- resources/views/livewire/like.blade.php
+++ resources/views/livewire/like.blade.php
@@ -1,6 +1,6 @@
 <div>
     
-<p><button wire:click="toggleLike"><img src="/img/moai@2x.png" alt="いいねボタン"></button></p>
+<p><button wire:click="toggleLike"><img src="/img/{{ $file }}" alt="いいねボタン"></button></p>
 <p>{{$count}}</p>
 
 </div>
