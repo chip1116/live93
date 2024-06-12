46d1371f9430eb2382cbc1e9708db6d97a971d39
RIE <chip.014868@gmail.com>
2024-06-12 11:18:37 +0900

    main,comment

--- resources/views/user/detail-screen.blade.php
+++ resources/views/user/detail-screen.blade.php
@@ -21,7 +21,7 @@
                     @foreach($rank as $item)
                     <li><p class="lank">{{$loop->iteration}}位</p><a href="{{ route('user.detail-main', [$item->id]) }}">
                         <div class="container">
-                            <div class="magazin-image"><img src="{{ asset('img/'.$item->post_image) }}" alt="Image" class="image"></div>
+                            <div class="magazin-image"><img src="{{ asset('img/'.$item->store_img) }}" alt="Image" class="image"></div>
                            
                             <div class="container-wrapper">
                             <h3 class="container-title"><span>{{$item->name}}</span></h3>
@@ -31,16 +31,15 @@
                            
 
                             <ul class="access">
-                                <li>アクセス:宮崎空港から車で35分</li>
-                                <li>住所:〒887-0101宮崎県日南市宮浦</li>
-                                <li>0987-29-1001</li>
+                                <li>{{ $item->access}}</li>
+                                <li>住所:{{ $item->postal_code }}{{ $item->location->name }}{{ $item->address_level3 }}</li>
+                                <li>{{ $item->tel }}</li>
                             </ul>
                     
                             </div>
                             <h4 class="coment-title"><span>てげよかポイント</span></h4>
                             <div class="comment-box">
-                                <p>とっても珍しい鍾乳洞の中にある鵜戸神宮は
-                                    めちゃくちゃオーラのあるパワースポットです！</p>
+                                <p>{{ $item->store_comment }}</p>
                             </div>
                         </div>
                         </a></li>
@@ -85,8 +84,7 @@
                         </div>
                         <h4 class="coment-title"><span>てげよかポイント</span></h4>
                         <div class="comment-box">
-                            <p>とっても珍しい鍾乳洞の中にある鵜戸神宮は
-                                めちゃくちゃオーラのあるパワースポットです！</p>
+                        <p>{{ $item->store_comment }}</p>
                         </div>
                     </div>
                 </a></li>
